daa719c794535a162e8bd2b8fc48515c992287cf
Francis Galiegue <fgaliegue@gmail.com>
2012-12-25 17:38:42 +0100

    Start a crude implementation of a URI template expression parser
    Really crude. No error checking etc...

---
+++ src/main/java/org/eel/kitchen/uritemplate/expression/Expression.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.uritemplate.expression;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Collection;
+
+public final class Expression
+{
+    private final String varName;
+
+    public Expression(final String varName)
+    {
+        this.varName = varName;
+    }
+
+    public Collection<String> getVarNames()
+    {
+        return ImmutableSet.of(varName);
+    }
+}
