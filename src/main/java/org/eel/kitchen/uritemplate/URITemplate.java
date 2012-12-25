f061bec52e13cd6ccc49723df3c2103ac3c86116
Francis Galiegue <fgaliegue@gmail.com>
2012-12-25 13:09:11 +0100

    Don't accept unpaired brackets in constructor
    And depend on guava -- again.

--- src/main/java/org/eel/kitchen/uritemplate/URITemplate.java
+++ src/main/java/org/eel/kitchen/uritemplate/URITemplate.java
@@ -1,13 +1,24 @@
 package org.eel.kitchen.uritemplate;
 
+import com.google.common.base.CharMatcher;
+
+import java.util.regex.Pattern;
+
 public final class URITemplate
 {
+    private static final CharMatcher BRACKETS = CharMatcher.anyOf("{}");
+    private static final Pattern PATTERN = Pattern.compile("\\{[^{}]*\\}");
     private final String tmpl;
 
     public URITemplate(final String tmpl)
+        throws InvalidTemplateException
     {
         if (tmpl == null)
             throw new NullPointerException("null template not accepted");
+
+        final String withoutTemplates = PATTERN.matcher(tmpl).replaceAll("");
+        if (BRACKETS.matchesAnyOf(withoutTemplates))
+            throw new InvalidTemplateException("unpaired brackets");
         this.tmpl = tmpl;
     }
 }
--- src/test/java/org/eel/kitchen/uritemplate/URITemplateTest.java
+++ src/test/java/org/eel/kitchen/uritemplate/URITemplateTest.java
@@ -8,12 +8,24 @@ public final class URITemplateTest
 {
     @Test
     public void nullTemplateStringIsNotAccepted()
+        throws InvalidTemplateException
     {
         try {
             new URITemplate(null);
             fail("No exception thrown!");
         } catch (NullPointerException e) {
             assertEquals(e.getMessage(), "null template not accepted");
+        }
+    }
+
+    @Test
+    public void nonPairedBracketPairsAreNotAccepted()
+    {
+        try {
+            new URITemplate("{");
+            fail("No exception thrown!");
+        } catch (InvalidTemplateException e) {
+            assertEquals(e.getMessage(), "unpaired brackets");
         }
     }
 }
