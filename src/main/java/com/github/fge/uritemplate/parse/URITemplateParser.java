a3050de4c061045425e6c537950c65ff80806584
Francis Galiegue <fgaliegue@gmail.com>
2013-03-14 16:20:21 +0100

    More infrastructure code

--- src/main/java/com/github/fge/uritemplate/parse/URITemplateParser.java
+++ src/main/java/com/github/fge/uritemplate/parse/URITemplateParser.java
@@ -1,12 +1,36 @@
 package com.github.fge.uritemplate.parse;
 
+import com.github.fge.uritemplate.URITemplateException;
 import com.github.fge.uritemplate.expression.URITemplateExpression;
 import com.google.common.collect.Lists;
 
+import java.nio.CharBuffer;
 import java.util.List;
 
 public final class URITemplateParser
 {
     private final List<URITemplateExpression> expressions
         = Lists.newArrayList();
+
+    public void parse(final String input)
+        throws URITemplateException
+    {
+        final CharBuffer buffer = CharBuffer.wrap(input);
+
+        ExpressionParser expressionParser;
+        URITemplateExpression expression;
+
+        while (buffer.hasRemaining()) {
+            expressionParser = selectParser(buffer);
+            if (expressionParser == null)
+                throw new URITemplateException();
+            expression = expressionParser.parse(buffer);
+            expressions.add(expression);
+        }
+    }
+
+    private static ExpressionParser selectParser(final CharBuffer buffer)
+    {
+        return null;
+    }
 }
