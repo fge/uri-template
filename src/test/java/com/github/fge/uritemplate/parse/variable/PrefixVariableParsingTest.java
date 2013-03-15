739dd051b775a9a8370583c192d96ccdce28860c
Francis Galiegue <fgaliegue@gmail.com>
2013-03-15 19:38:52 +0100

    Start to test prefix variable parsing
    Only successful cases for now.

---
+++ src/test/java/com/github/fge/uritemplate/parse/variable/PrefixVariableParsingTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.uritemplate.parse.variable;
+
+import com.github.fge.uritemplate.URITemplateParseException;
+import com.github.fge.uritemplate.parse.VariableSpecParser;
+import com.github.fge.uritemplate.vars.PrefixVariable;
+import com.github.fge.uritemplate.vars.VariableSpec;
+import com.google.common.collect.Lists;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.nio.CharBuffer;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public final class PrefixVariableParsingTest
+{
+    @DataProvider
+    public Iterator<Object[]> validInputs()
+    {
+        final List<Object[]> list = Lists.newArrayList();
+
+        String input;
+
+        input = "foo:323";
+        list.add(new Object[]{ input });
+
+        input = "%33foo:323";
+        list.add(new Object[]{ input });
+
+        input = "foo%20:323";
+        list.add(new Object[]{ input });
+
+        input = "foo_%20bar:323";
+        list.add(new Object[]{ input });
+
+        input = "FoOb%02ZAZE287:323";
+        list.add(new Object[]{ input });
+
+        input = "foo.bar:323";
+        list.add(new Object[]{ input });
+
+        input = "foo_%20bar.baz%af.r:323";
+        list.add(new Object[]{ input });
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validInputs")
+    public void simpleVariablesAreParsedCorrectly(final String input)
+        throws URITemplateParseException
+    {
+        final CharBuffer buffer = CharBuffer.wrap(input).asReadOnlyBuffer();
+        final VariableSpec varspec = VariableSpecParser.parse(buffer);
+
+        assertEquals(varspec.getName(),
+            input.substring(0, input.indexOf(':')));
+        assertSame(varspec.getClass(), PrefixVariable.class,
+            "unexpected class for parsed variable");
+    }
+}
