07737164ea246f6f21f20bcfefaaeacc4036a546
Francis Galiegue <fgaliegue@gmail.com>
2013-05-20 23:42:46 +0200

    Start and add list expansion tests

---
+++ src/test/java/com/github/fge/uritemplate/expand2/ListsExpansionTest.java
@@ -0,0 +1,13 @@
+package com.github.fge.uritemplate.expand2;
+
+import java.io.IOException;
+
+public final class ListsExpansionTest
+    extends AbstractExpansionTest
+{
+    public ListsExpansionTest()
+        throws IOException
+    {
+        super("lists");
+    }
+}
