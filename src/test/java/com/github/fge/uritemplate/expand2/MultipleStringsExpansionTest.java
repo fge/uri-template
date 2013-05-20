6ff01ea937d064c14456dc78d5878b180e10d480
Francis Galiegue <fgaliegue@gmail.com>
2013-05-20 21:36:28 +0200

    Add multiple strings expansion tests

---
+++ src/test/java/com/github/fge/uritemplate/expand2/MultipleStringsExpansionTest.java
@@ -0,0 +1,13 @@
+package com.github.fge.uritemplate.expand2;
+
+import java.io.IOException;
+
+public final class MultipleStringsExpansionTest
+    extends AbstractExpansionTest
+{
+    public MultipleStringsExpansionTest()
+        throws IOException
+    {
+        super("multipleStrings");
+    }
+}
