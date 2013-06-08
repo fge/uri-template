89968cd1a371d0868715e535034a3b809c632526
Francis Galiegue <fgaliegue@gmail.com>
2013-06-08 18:10:15 +0200

    More code for VariableMap{,Builder}

--- src/main/java/com/github/fge/uritemplate/vars/VariableMap.java
+++ src/main/java/com/github/fge/uritemplate/vars/VariableMap.java
@@ -1,12 +1,24 @@
 package com.github.fge.uritemplate.vars;
 
 import com.github.fge.Frozen;
+import com.github.fge.uritemplate.vars.values.VariableValue;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 public final class VariableMap
     implements Frozen<VariableMapBuilder>
 {
+    final Map<String, VariableValue> vars;
+
     VariableMap(final VariableMapBuilder builder)
     {
+        vars = ImmutableMap.copyOf(builder.vars);
+    }
+
+    public static VariableMapBuilder newBuilder()
+    {
+        return new VariableMapBuilder();
     }
 
     @Override
--- src/main/java/com/github/fge/uritemplate/vars/VariableMapBuilder.java
+++ src/main/java/com/github/fge/uritemplate/vars/VariableMapBuilder.java
@@ -1,12 +1,38 @@
 package com.github.fge.uritemplate.vars;
 
 import com.github.fge.Thawed;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.serviceloader.MessageBundles;
+import com.github.fge.uritemplate.URITemplateMessageBundle;
+import com.github.fge.uritemplate.vars.values.ScalarValue;
+import com.github.fge.uritemplate.vars.values.VariableValue;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
 
 public final class VariableMapBuilder
     implements Thawed<VariableMap>
 {
+    private static final MessageBundle BUNDLE
+        = MessageBundles.forClass(URITemplateMessageBundle.class);
+
+    final Map<String, VariableValue> vars = Maps.newHashMap();
+
+    VariableMapBuilder()
+    {
+    }
+
     VariableMapBuilder(final VariableMap variableMap)
     {
+        vars.putAll(variableMap.vars);
+    }
+
+    public VariableMapBuilder setScalar(final String varname,
+        final Object value)
+    {
+        vars.put(BUNDLE.checkNotNull(varname, "varmap.nullVarName"),
+            new ScalarValue(value));
+        return this;
     }
 
     @Override
