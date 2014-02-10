9b4beaf5ce25f1465b8720af89a6975fdcd6346d
Francis Galiegue <fgaliegue@gmail.com>
2014-02-10 13:37:23 +0100

    EscapeCharsets: add comments
    Even though it is a package local class only.

--- src/main/java/com/github/fge/uritemplate/render/EscapeCharsets.java
+++ src/main/java/com/github/fge/uritemplate/render/EscapeCharsets.java
@@ -1,11 +1,24 @@
 package com.github.fge.uritemplate.render;
 
+import com.google.common.net.PercentEscaper;
+
+/**
+ * Character sets to escape during URI template expansions
+ *
+ * <p>This makes uses of Guava's {@link PercentEscaper} and relies on the
+ * grammar defined in RFC 3986.</p>
+ */
 final class EscapeCharsets
 {
     private EscapeCharsets()
     {
     }
 
+    /*
+     * Note that the grammar also includes all of alphanumeric ASCII chars;
+     * however, PercentEscaper automatically considers such chars as safe: if
+     * you try and include them, you'll get an IllegalArgumentException.
+     */
     static final String UNRESERVED = "-._~";
     static final String RESERVED_PLUS_UNRESERVED = UNRESERVED
         + ":/?#[]@" // gen-delims
